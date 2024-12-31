1515edd95a0f74fbd8bf4af20e7de4287d41fdeb
Brandon Lee Truter <brandon.truter@ncino.com>
2024-12-31 14:19:55 +0200

    simplify store logic with vuex map helpers

--- vuex-app/src/components/ProductList.vue
+++ vuex-app/src/components/ProductList.vue
@@ -5,13 +5,15 @@
     <ul v-else>
       <li v-for="product in products">
         {{ product.title }} - {{ product.price | currency }} - {{product.inventory}}
-        <button @click="addProductToCart(product)">Add to Cart</button>
+        <button @click="addProductToCart(product)" :disabled="!productIsInStock(product)">Add to Cart</button>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+  import {mapState, mapGetters, mapActions} from "vuex";
+
   export default {
     data() {
       return {
@@ -20,20 +22,25 @@
     },
 
     computed: {
-      products() {
-        return this.$store.getters.availableProducts
-      }
+      ...mapState({
+        products: state => state.products
+      }),
+
+      ...mapGetters({
+        productIsInStock: 'productIsInStock'
+      })
     },
 
     methods: {
-      addProductToCart(product) {
-        this.$store.dispatch('addProductToCart', product)
-      }
+      ...mapActions({
+        fetchProducts: 'fetchProducts',
+        addProductToCart: 'addProductToCart'
+      })
     },
 
     created() {
       this.loading = true
-      this.$store.dispatch('fetchProducts')
+      this.fetchProducts()
         .then(() => {
           this.loading = false
         })
--- vuex-app/src/components/ShoppingCart.vue
+++ vuex-app/src/components/ShoppingCart.vue
@@ -6,25 +6,29 @@
         {{ product.title }} - {{ product.price | currency }} - {{ product.quantity }}
       </li>
     </ul>
-    <p>
-      Total: ${{ total }}
-    </p>
-    <button @click="$store.dispatch('checkout')">Checkout</button>
-    <p v-if="$store.state.checkoutStatus">
-      {{$store.state.checkoutStatus}}
-    </p>
+    <p>Total: ${{ total }}</p>
+    <button @click="checkout">Checkout</button>
+    <p v-if="checkoutStatus">{{checkoutStatus}}</p>
   </div>
 </template>
 
 <script>
+  import {mapState, mapGetters, mapActions} from "vuex";
+
   export default {
     computed: {
-      products() {
-        return this.$store.getters.cartProducts
-      },
-      total() {
-        return this.$store.getters.cartTotal
-      }
+      ...mapGetters({
+        products: 'cartProducts',
+        total: 'cartTotal'
+      }),
+
+      ...mapState({
+        checkoutStatus: 'checkoutStatus'
+      })
+    },
+
+    methods: {
+      ...mapActions(['checkout'])
     }
   }
 </script>
